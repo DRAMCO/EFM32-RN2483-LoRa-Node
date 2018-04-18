324980fec05a993cb6956f1de45b34dd26c71188
Gilles Callebaut <callebaut.gilles@gmail.com>
2018-04-18 15:37:01 +0200

    update default data rate info

--- src/my_lora_device.h
+++ src/my_lora_device.h
@@ -27,17 +27,32 @@
  */
 #define OVER_THE_AIR_ACTIVATION		1
 
-
+/*
+ * SET default data rate
+ * A higher SF yields a longer air time and thus energy
+ * but extends the range
+ *
+ * Options:
+ * SF12_BW125
+ * SF11_BW125
+ * SF10_BW125
+ * SF9_BW125
+ * SF8_BW125
+ * SF7_BW125
+ *
+ * Try changing the SF to see its impact on the energy consumption
+ */
 #define DEFAULT_DATA_RATE			SF8_BW125
+
 
 #if OVER_THE_AIR_ACTIVATION == 1
 // Copy your settings here
 /* 64-bit device ID */
-#define LORAWAN_DEVICE_EUI			"00D1098D1AEF0A52"
+#define LORAWAN_DEVICE_EUI			"0056FFEA1D531DC7"
 /* 64-bit application ID */
-#define LORAWAN_APPLICATION_EUI     "70B3D57EF0006CA6"
+#define LORAWAN_APPLICATION_EUI     "70B3D57ED000B816"
 /* 128-bit AES key */
-#define LORAWAN_APPLICATION_KEY     "2A5B8F6B478DD2C4C2D259C98274A7DC"
+#define LORAWAN_APPLICATION_KEY     "990A948D12DADF5D90C90EFF0BDBDE66"
 // Don't change
 #define JOIN_MECHANISM				OTAA
 
